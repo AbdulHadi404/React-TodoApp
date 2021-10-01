d175aeb07fc75a2c0b8e64cc58db6f6b3d20c4d5
Abdul Hadi <abdulhadi8369@gmail.com>
2021-10-01 19:00:27 +0500

    initial commit
    Created Card Component for Todos

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoCard from './components/TodoCard';
+
+const App = () => {
+  return (
+    <div className="App">
+      <TodoCard />
+    </div>
+  );
+}
+
+export default App;
